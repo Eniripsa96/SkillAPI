88ddf58092f0392c4d8dbb5f45794c98ad5f8992
Steven Sucy <sms6462@g.rit.edu>
2015-01-16 22:53:57 -0500

    Added Particle mechanic

---
+++ src/com/sucy/skill/dynamic/mechanic/ParticleMechanic.java
@@ -0,0 +1,35 @@
+package com.sucy.skill.dynamic.mechanic;
+
+import com.sucy.skill.api.util.ParticleHelper;
+import com.sucy.skill.dynamic.EffectComponent;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.List;
+
+/**
+ * Plays a particle effect
+ */
+public class ParticleMechanic extends EffectComponent
+{
+    /**
+     * Executes the component
+     *
+     * @param caster  caster of the skill
+     * @param level   level of the skill
+     * @param targets targets to apply to
+     *
+     * @return true if applied to something, false otherwise
+     */
+    @Override
+    public boolean execute(LivingEntity caster, int level, List<LivingEntity> targets)
+    {
+        if (targets.size() == 0) return false;
+
+        for (LivingEntity target : targets)
+        {
+            ParticleHelper.play(target.getLocation(), settings);
+        }
+
+        return true;
+    }
+}

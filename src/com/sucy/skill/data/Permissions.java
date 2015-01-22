faaa689a1ad03db88f8e0c8425e74380d281fd12
Steven Sucy <sms6462@g.rit.edu>
2015-01-22 04:01:02 -0500

    Added permissions

--- src/com/sucy/skill/data/Permissions.java
+++ src/com/sucy/skill/data/Permissions.java
@@ -4,8 +4,10 @@ public class Permissions
 {
     private static final String ROOT = "skillapi.";
 
-    public static final String BASIC = ROOT + "basic";
-    public static final String EXP   = ROOT + "exp";
-    public static final String LVL   = ROOT + "level";
-    public static final String SKILL = ROOT + "skill";
+    public static final String BASIC  = ROOT + "basic";
+    public static final String EXP    = ROOT + "exp";
+    public static final String LVL    = ROOT + "level";
+    public static final String MANA   = ROOT + "mana";
+    public static final String POINTS = ROOT + "points";
+    public static final String SKILL  = ROOT + "skill";
 }

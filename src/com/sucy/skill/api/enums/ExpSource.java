de78dd123292319fd7185407d386f2dc377cbcb9
Steven Sucy <sms6462@g.rit.edu>
2015-01-22 04:01:36 -0500

    Added more exp sources

--- src/com/sucy/skill/api/enums/ExpSource.java
+++ src/com/sucy/skill/api/enums/ExpSource.java
@@ -15,32 +15,37 @@ public enum ExpSource
     /**
      * Experience resulted from breaking a block
      */
-    BLOCK_BREAK(0x10),
+    BLOCK_BREAK(0x2),
 
     /**
      * Experience resulted from placing a block
      */
-    BLOCK_PLACE(0x100),
+    BLOCK_PLACE(0x4),
 
     /**
      * Experience resulted from crafting an item
      */
-    CRAFT(0x1000),
+    CRAFT(0x8),
 
     /**
      * Experience resulted from an issued command
      */
-    COMMAND(0x10000),
+    COMMAND(0x10),
 
     /**
      * Experience resulted from an unspecified reason
      */
-    SPECIAL(0x100000),
+    SPECIAL(0x20),
 
     /**
-     * Experience resulted from initializing the player data on startup
+     * Experience from Bottles o' Enchanting
      */
-    INITIALIZATION(0x1000000);
+    EXP_BOTTLE(0x40),
+
+    /**
+     * Experience from smelting ore
+     */
+    SMELT(0x80);
 
     /**
      * The ID of the experience source which should be a unique power of 2 (or bit value)

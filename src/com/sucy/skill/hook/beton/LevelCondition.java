c1271edc03b22a25deb6606870d2036222ba4ff0
trase2482 <maniac2482@hotmail.com>
2015-08-13 21:08:01 -0400

    Update LevelCondition.java
    Fix for BetonQuest conditions for new version.

--- src/com/sucy/skill/hook/beton/LevelCondition.java
+++ src/com/sucy/skill/hook/beton/LevelCondition.java
@@ -1,57 +1,53 @@
 package com.sucy.skill.hook.beton;
 
 import com.sucy.skill.SkillAPI;
-import com.sucy.skill.api.player.PlayerClass;
 import com.sucy.skill.api.player.PlayerData;
 import org.bukkit.entity.Player;
 import pl.betoncraft.betonquest.api.Condition;
 import pl.betoncraft.betonquest.utils.PlayerConverter;
 
+import java.util.HashMap;
 import pl.betoncraft.betonquest.InstructionParseException;
 
 /**
- * Beton Quest condition for a player being a certain level
- * In the format Level num
- * 
- * For example, to check for a player higher level than 10:
- * is_ten: Level 10
+ * Beton Quest condition for a player being a specific class in the format
+ * 'Class name'
+ *
+ * For example, the condition would look like is_cleric: Class Cleric
  */
-public class LevelCondition extends Condition
-{
-    private static final String LEVEL = "level";
-    private static final String GROUP = "group";
-    private static final String MIN   = "min";
+public class SkillCondition extends Condition {
 
-    String group;
-    int    level;
-    boolean min = false;
-   
-    public LevelCondition(String playerID, String instructions) throws InstructionParseException
-    {
+    String skillName = "";
+
+    public SkillCondition(String playerID, String instructions) throws InstructionParseException {
         super(playerID, instructions);
-     
-        String[] parse = instructions.split(" ");
-        if (parse.length < 2) {
-            throw new InstructionParseException("Please specify a minimum level");
+
+        String[] parts = instructions.split(" ");
+        if (parts.length < 2) {
+            throw new InstructionParseException("Skill Condition: Skill name name... name");
         }
-        level = Integer.parseInt(parse[1]);
+
+        for (int i = 1; i < parts.length; i++) {
+            if (i == 1) {
+                skillName += parts[i];
+            } else {
+                skillName += " " + parts[i];
+            }
+        }
 
     }
-    
+
     @Override
-    public boolean check(String playerID)
-    {
+    public boolean check(String playerID) {
+
         Player player = PlayerConverter.getPlayer(playerID);
         PlayerData data = SkillAPI.getPlayerData(player);
-        PlayerClass playerClass = data.getClass(group);
-        if (playerClass == null) playerClass = data.getMainClass();
-        //Check if player is over the specified level
-        if (playerClass.getLevel() >= level) 
-        {
+
+        if (data.getSkill(skillName) != null) {
             return true;
         }
+
         return false;
-        
     }
 
 }

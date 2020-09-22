5df89989b498be8b8c1bfe76d7feec82b5162e19
Sentropic <60368610+Sentropic@users.noreply.github.com>
2020-09-22 12:35:22 -0600

    Added back target folder Deleted accidentally

---
+++ src/main/java/com/sucy/skill/dynamic/target/AreaTarget.java
@@ -0,0 +1,78 @@
+/**
+ * SkillAPI
+ * com.sucy.skill.dynamic.target.AreaTarget
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Steven Sucy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software") to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.sucy.skill.dynamic.target;
+
+import com.sucy.skill.api.util.Nearby;
+import com.sucy.skill.cast.IIndicator;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Applies child components to the closest all nearby entities around
+ * each of the current targets.
+ */
+public class AreaTarget extends TargetComponent {
+    private static final String RADIUS = "radius";
+    private static final String RANDOM = "random";
+
+    private final Random random = new Random();
+
+    /** {@inheritDoc} */
+    @Override
+    List<LivingEntity> getTargets(
+            final LivingEntity caster, final int level, final List<LivingEntity> targets) {
+
+        final double radius = parseValues(caster, RADIUS, level, 3.0);
+        final boolean random = settings.getBool(RANDOM, false);
+        return determineTargets(caster, level, targets, t -> shuffle(Nearby.getLivingNearby(t.getLocation(), radius), random));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    void makeIndicators(final List<IIndicator> list, final Player caster, final LivingEntity target, final int level) {
+        makeCircleIndicator(list, target, parseValues(caster, RADIUS, level, 3.0));
+    }
+
+    @Override
+    public String getKey() {
+        return "area";
+    }
+
+    private List<LivingEntity> shuffle(final List<LivingEntity> targets, final boolean random) {
+        if (!random) return targets;
+
+        final List<LivingEntity> list = new ArrayList<>();
+        while (!targets.isEmpty()) {
+            list.add(targets.remove(this.random.nextInt(targets.size())));
+        }
+        return list;
+    }
+}

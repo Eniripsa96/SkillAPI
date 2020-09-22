77a89e65df25d66432b6b2556517f8830db352d4
Sentropic <60368610+Sentropic@users.noreply.github.com>
2020-09-22 11:13:54 -0600

    Updated to supported WE class BlockVector3

--- src/main/java/com/sucy/skill/hook/WorldGuardHook.java
+++ src/main/java/com/sucy/skill/hook/WorldGuardHook.java
@@ -1,7 +1,7 @@
 package com.sucy.skill.hook;
 
 import com.google.common.collect.ImmutableList;
-import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
@@ -46,8 +46,8 @@ public class WorldGuardHook {
         }
     }
 
-    private static Vector asVector(final Location location) {
-        return new Vector(location.getX(), location.getY(), location.getZ());
+    private static BlockVector3 asVector(final Location location) {
+        return BlockVector3.at(location.getX(), location.getY(),location.getZ());
     }
 
     private static Method getRegionMethod() throws Exception {

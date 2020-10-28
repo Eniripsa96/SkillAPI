5ef6dbafdcd2ccc21dd861c0b4f7189521d364cf
Sentropic <60368610+Sentropic@users.noreply.github.com>
2020-10-28 01:05:25 -0600

    Added missing Minecraft versions

--- editor/js/data/data.js
+++ editor/js/data/data.js
@@ -8,6 +8,9 @@ depend('data/1.10', function() { if (version === '10') DATA = DATA_10 });
 depend('data/1.11', function() { if (version === '11') DATA = DATA_11 });
 depend('data/1.12', function() { if (version === '12') DATA = DATA_12 });
 depend('data/1.13', function() { if (version === '13') DATA = DATA_13 });
+depend('data/1.14', function() { if (version === '14') DATA = DATA_14 });
+depend('data/1.15', function() { if (version === '15') DATA = DATA_15 });
+depend('data/1.16', function() { if (version === '16') DATA = DATA_16 });
 
 function getMaterials() {
     return DATA.MATERIALS;

cb1c8b8e203af4295d8c2647b974afbbe87785d4
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-12-16 15:24:02 -0800

    Trunc literally exists by default lol

--- src/main/java/com/sucy/skill/data/formula/Formula.java
+++ src/main/java/com/sucy/skill/data/formula/Formula.java
@@ -62,7 +62,6 @@ public class Formula implements IValue
         put("abs", Abs.class);
         put("ceil", Ceil.class);
         put("round", Round.class);
-        put("trunc", Trunc.class);
         put("cos", Cos.class);
         put("floor", Floor.class);
         put("sqrt", Root.class);

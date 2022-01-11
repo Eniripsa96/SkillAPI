f8a647366fbb523fd74dca6a5e99c1e708aa62ae
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-01-10 23:41:35 -0800

    Make regen conservative with events

--- src/main/java/com/sucy/skill/api/player/PlayerData.java
+++ src/main/java/com/sucy/skill/api/player/PlayerData.java
@@ -1512,15 +1512,17 @@ public class PlayerData {
 	
 	// Only bring Player as param because playerdata only has offlineplayer
 	public void regenHealth(Player p) {
-		if (!p.isDead()) {
+		if (!p.isDead() && p.getHealth() != p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue()) {
 			double regen = Math.max(0, (SkillAPI.getPlayerData(p).scaleStat(AttributeManager.HEALTH_REGEN, 0)) * 0.1);
 			
-			PlayerRegenEvent event = new PlayerRegenEvent(p, regen);
-			Bukkit.getPluginManager().callEvent(event);
-			if (!event.isCancelled()) {
-				regen = event.getAmount();
-		        double health = p.getHealth();
-		        p.setHealth(Math.min(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue(), health + regen));
+			if (regen > 0) {
+				PlayerRegenEvent event = new PlayerRegenEvent(p, regen);
+				Bukkit.getPluginManager().callEvent(event);
+				if (!event.isCancelled()) {
+					regen = event.getAmount();
+			        double health = p.getHealth();
+			        p.setHealth(Math.min(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue(), health + regen));
+				}
 			}
 		}
 	}

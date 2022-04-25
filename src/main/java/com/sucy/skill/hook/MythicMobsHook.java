1fe15a6c98440b4e823f5bb68c62ee430817fdab
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-04-25 00:37:04 -0700

    Update to 1.18 MM

--- src/main/java/com/sucy/skill/hook/MythicMobsHook.java
+++ src/main/java/com/sucy/skill/hook/MythicMobsHook.java
@@ -1,7 +1,7 @@
 package com.sucy.skill.hook;
 
-import io.lumine.xikage.mythicmobs.MythicMobs;
 import org.bukkit.entity.LivingEntity;
+import io.lumine.mythic.bukkit.MythicBukkit;
 
 /**
  * SkillAPI © 2017
@@ -11,14 +11,14 @@ public class MythicMobsHook {
 
     public static void taunt(final LivingEntity target, final LivingEntity source, final double amount) {
         if (amount > 0) {
-            MythicMobs.inst().getAPIHelper().addThreat(target, source, amount);
+            MythicBukkit.inst().getAPIHelper().addThreat(target, source, amount);
         }
         else if (amount < 0) {
-            MythicMobs.inst().getAPIHelper().reduceThreat(target, source, -amount);
+        	MythicBukkit.inst().getAPIHelper().reduceThreat(target, source, -amount);
         }
     }
 
     public static boolean isMonster(final LivingEntity target) {
-        return MythicMobs.inst().getAPIHelper().isMythicMob(target);
+        return MythicBukkit.inst().getAPIHelper().isMythicMob(target);
     }
 }

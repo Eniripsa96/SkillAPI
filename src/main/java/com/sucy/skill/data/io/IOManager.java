62237d6c8cec1052d96f168beb23b81be9a68db1
Alex Z <alexz1012@yahoo.com>
2022-08-03 16:07:44 -0700

    stop throwing account 1 not professed

--- src/main/java/com/sucy/skill/data/io/IOManager.java
+++ src/main/java/com/sucy/skill/data/io/IOManager.java
@@ -299,9 +299,6 @@ public abstract class IOManager
     {
         try
         {
-        	if (data.getData(1).getClass("class") == null || data.getData(1).getClass("class").getData().getName() == null) {
-        		throw new Exception("Account 1 was not professed");
-        	}
             DataSection file = new DataSection();
             file.set(LIMIT, data.getAccountLimit());
             file.set(ACTIVE, data.getActiveId());
